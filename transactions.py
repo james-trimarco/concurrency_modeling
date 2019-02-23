18c7d50c45002b845d0323f37bafaed616800fae
James Trimarco <james.trimarco@gmail.com>
2019-02-23 09:27:02 -0500

    removed some unecessary lists

--- transactions.py
+++ transactions.py
@@ -1,12 +1,35 @@
 """
 Author: James Trimarco
 
-Data Storage and Retrieval, Homework 9, Question 2
+When we have two schedules of reads and writes — let's call them transactions 
+T_1 and T_2 — we can lose information if reads and writes don't take place in a 
+certain order. For example, T_1 might read and modify file X, then T_2 reads 
+and modifies the same file, but then T_1 saves its modifications. So T_2's 
+changes are lost. 
 
-Write a Python program to answer this exercise using Algorithm 20.1. 
-Your program should accept any two transactions and output all schedules. 
-It should label each serializable schedule with a list of all 
-equivalent serial schedules.
+The following program finds all combinations of two schedules and provides 
+information on which ones are safe. It takes two schedules as input in
+the following format:
+
+    "read_item(X); 
+    X:= X-N; 
+    write_item(X); 
+    read_item(Y); 
+    Y:= Y+N;
+    write_item(Y)"
+
+The output includes every possible schedule resulting from their combination, 
+along with information on whether the schedule is _serializable_ or not. A 
+transaction is serializable and free of read/write conflicts if it's logically 
+equivalent to one in which the two transactions ran one after the other, 
+with no interleaving. 
+
+The program figures out whether a transaction is serializable by modeling
+it with a simple two-node graph structure and then searching for cycles in the 
+graph. This is the standard algorithm described by Elmasri and Navathe in 
+"Fundamentals of Database Systems." If it finds no cycles, the transaction 
+is serializable. 
+
 """
 
 import re # for regex matching
@@ -17,12 +40,9 @@ import sys
 read_str = "read_item"
 write_str = "write_item"
 
-# Creates boolean value for test that Python major version > 2
-py3 = version_info[0] > 2
-
-# Checks python version and prompts for search
-if not py3:
-    print("Please type 'scl enable rh-python36 bash' to turn on Python 3.")
+# Checks python version and redirects users on Python 2
+if version_info[0] < 2:
+    print("Please type 'scl enable rh-python36 bash' to activate Python 3.")
     sys.exit()
 
 # Define functions
@@ -36,24 +56,28 @@ def combine_ordered(l1, l2):
     if l1 == []: return [l2]
     if l2 == []: return [l1]
     
-    # Break lists into front and back
+    # Break lists into first element and all the rest 
     x, *l1_tail = l1
     y, *l2_tail = l2
     
-    # Recurse to generate all legal combinations
+    # Recurse to generate all ordered combinations
     return [ [x] + l for l in combine_ordered(l1_tail, l2) ] + \
            [ [y] + l for l in combine_ordered(l2_tail, l1) ]
+
 
 def start_program():
     """
     This function provides the user with the option to select default 
     transactions or supply their own. 
     """
+    # Default schedules
     tr1 = "read_item(X); X:= X-N; write_item(X); read_item(Y); Y:= Y+N;write_item(Y)" 
     tr2 = "read_item(X); X:= X + M; write_item(X);"
+    
     while True:
-        user_input = input("Would you like to use the default transactions or"
-                         "suppy your own? (1 for defaults, 2 for custom)\n").lower()
+        user_input = input("Would you like to use the default transactions or "
+                           "suppy your own? (1 for defaults, 2 for custom)\n") \
+                           .lower()
         
         try:
             user_input = int(user_input)
@@ -61,76 +85,134 @@ def start_program():
             continue
         
         if user_input == 1:
-            T_1 = list(filter(None, tr1.split(';'))) 
+            T_1 = list(filter(None, tr1.split(';'))) # split up default at semicolons
             T_2 = list(filter(None, tr2.split(';'))) 
             output_all_schedules(T_1, T_2)
             return
         elif user_input == 2:
-            T_1 = get_transaction_1()
-            T_2 = get_transaction_2()
+            T_1 = get_transaction("1") # get two user-supplied transaction texts
+            T_2 = get_transaction("2")
             output_all_schedules(T_1, T_2)
             return
         elif user_input not in (1, 2):
             continue
+        
             
-def get_transaction_1():
+def get_transaction(ord):
     """
-    Prompts the reader for transaction one.
+    Prompts the reader for transactions one and two.
     """
     while True:
-        input_string = input("Please enter transaction one as a single string "
-                             "with semicolons between commands.\n").lower()
+        input_string = input("Please enter transaction {ord} as a single string "
+                             "with semicolons between commands. Your transaction " 
+                             "should contain the text 'read_item'.\n" \
+                             .format(ord=ord)).lower()
 
         if "read_item" in input_string:
-            # split at semicolon
-            T_1 = list(filter(None, input_string.split(';')))
+            T_1 = list(input_string.split(';')) # split at semicolon
             return T_1
         else:
             continue
+        
 
-def get_transaction_2():
+def sch_to_dict(schedule, idx):
     """
-    Prompts the reader for transaction two.
+    This function takes a schedule as text and returns a dictionary with all 
+    of its relevant information.
     """
-    while True:
-        input_string = input("Please enter transaction two as a single string "
-                             "with semicolons between commands.\n").lower()
-        if "read_item" in input_string:
-            # split at semicolon
-            T_2 = list(filter(None, input_string.split(';'))) 
-            return T_2
-        else:
-            continue
+    sch = []
+    s = re.compile("(?<=\()(.*?)(?=\))") # extract a string from between parentheses
+    for idx, item in enumerate(schedule):
+        
+        if read_str in item or write_str in item: # spot transactions
+            obj = s.search(item).group()
+            t = {
+                    'tr': item[:2],
+                    'command': item[3:],
+                    'order': idx + 1,
+                    'obj': obj
+                    }
+            sch.append(t)
+    sch = sorted(sch, key=lambda k: k['order'])
+    return(sch)
+    
 
-def build_lookup(schedules):
+def create_digraph(schedule):
     """
-    Runs the logic for determining which graphs are serializable, then 
-    builds a simple table of summary information on each schedule to 
-    inform output. 
+    This function takes a dictionary and returns a simple directed graph 
+    structure with two nodes: T_1 and T_2. 
+    
+    Each node can contain an edge leading to the opposite node, or not. 
+    
+    The nested for loop here allows the function to look at the whole schedule
+    from the point of view of transaction one and then transaction two. 
     """
-    lib = []
-    # create a dictionary of serializabilities
+    graph = {"T1": [], # initialize graph with two nodes
+             "T2": []}
+             
+    for t in graph: # loop twice, once for each transaction
+        write_token = [] # initialize token for writes
+        read_token = [] # initialize token for reads
+        
+        if t == "T1": 
+            t_not = "T2"
+        else: 
+            t_not = "T1"
+            
+        for tr in schedule: # Loop through the schedule to examine logic
+            
+            if tr['tr'] == t: 
+                # If T_I has written, create write token
+                if write_str in tr['command']:
+                    write_token.append(tr['obj']) # Stores object of write as token
+                # If T_I has read, create read token
+                if read_str in tr['command']:
+                    read_token.append(tr['obj']) # Stores object of read as token
+            
+            if tr['tr'] != t: # Check for conditions that affect serializability
+                # check for write -> read
+                if read_str in tr['command'] and tr['obj'] in write_token:
+                    graph[t] = t_not
+                # Check for read -> write        
+                if write_str in tr['command'] and tr['obj'] in read_token:
+                    graph[t] = t_not
+                # Check for write -> write
+                if write_str in tr['command'] and tr['obj'] in write_token:
+                    graph[t] = t_not
+    return graph
+
+
+def check_serial(schedules):
+    """
+    This function accepts a list of schedules and returns a list of dicts with
+    information about their serializability. 
+    
+    Determines which directed graphs are serializable by checking them
+    for cycles. 
+    """
+    lib = [] # stores one dict for each possible schedule of transactions
+    
     for idx, schedule in enumerate(schedules):
-        lookup = {}
-        graph, sch = determine_serial(schedule, idx)
+        
+        sch = sch_to_dict(schedule, idx) # returns a dict
+        graph = create_digraph(sch) # returns a graph with nodes and edges
+        
         if graph['T1'] == 'T2' and graph['T2'] == 'T1':
             serializable = "No"
-            s_type = 0
-        if graph['T1'] == 'T2' and graph['T2'] != 'T1':
+        else:
             serializable = "Yes"
-            s_type = 1
-        if graph['T1'] != 'T2' and graph['T2'] == 'T1':
-            serializable = "Yes"
-            s_type = 2
+            
         lookup = {"idx": idx, 
-                  "serializable": serializable, 
-                  "s_type": s_type}
+                  "serializable": serializable}
         lib.append(lookup)
+        
     return(lib)
     
     
 def output_all_schedules(T_1, T_2):
     """
+    Accepts two transaction schedules and combines them.
+    
     Creates a list of every possible schedule, calls other functions to
     determine serializability, and prints results to the console. 
     """
@@ -146,28 +228,14 @@ def output_all_schedules(T_1, T_2):
     # create a list of legal combinations
     schedules = combine_ordered(T_1, T_2)
     
-    # create a dictionary of serializability status for all schedules
-    lib = build_lookup(schedules)
-        
-    # For each schedule
+    # create a dictionary of serializability statuses for all schedules
+    lib = check_serial(schedules)
+    
+    # For each possible schedule
     for idx, schedule in enumerate(schedules):
         T1_list = [] # list to store T1 transactions
         T2_list = [] # list to store T2 transactions
-        equivalents = [] # list to store nubmers of all equivalent schedules
-        
-        for entry in lib:
-            if entry['idx'] == idx:
-                current_data = entry
-                
-        for entry in lib:
-            if entry['s_type'] == current_data['s_type']:
-                equivalents.append(entry['idx'])
-        
-        
-        print('\n%s\nSchedule: %s' % (h_line, idx))
-        print('%-20s%-20s' % ("Transaction 1", "Transaction 2"))
-        print(h_line)
-        
+                  
         # for each transaction, separate T1s and T2s
         for item in schedule:
             if "T1" in item:
@@ -176,94 +244,26 @@ def output_all_schedules(T_1, T_2):
             if "T2" in item:
                 T2_list.append(item)
                 T1_list.append('')
+        
+        # Look up current schedule's status
+        for entry in lib:
+            if entry['idx'] == idx:
+                ans = entry['serializable']  
+                
+        # Print header
+        print('\n%s\nSchedule: %s' % (h_line, idx))
+        print('%-20s%-20s' % ("Transaction 1", "Transaction 2"))
+        print(h_line)
          
+        # Print all transactions in order 
         for i in range(0, len(schedule)):
             print('%-20s%-20s' % (T1_list[i][3:], T2_list[i][3:]))
             
-
+        # Print serializability
         print(h_line)
-        print("Serializable: " + current_data['serializable'])
+        print("Serializable: " + ans)
         
-        if current_data['serializable'] == 'Yes':
-            print("Equivalents")
-            for i in range(0, len(equivalents)):
-                if equivalents[i] != current_data['idx']:
-                    print(equivalents[i], ', ', end='', sep='')
-                if(i != 0 and i % 10 == 0):
-                    print("\n", end='', sep='')
         print("\n")
     return
-            
-    
-def create_digraph(schedule):
-    """
-    This function makes two large passes, one from the point of view of T1, 
-    the other from the point of view of T2. 
-    
-    Within each of these passes, the function loops through the entire schedule,
-    checking for conditions that allow it to draw a directed edge.
-    """
-    graph = {"T1": [],
-             "T2": []}
-             
-    l_1 = ["T1", "T2"]
-    for t in l_1: # loop twice, once for each T
-        write_token = [] # initialize token for condition 1
-        read_token = [] # initialize token for condition 2
-        
-        if t == "T1": 
-            t_not = "T2"
-        else: 
-            t_not = "T1"
-            
-        for tr in schedule: 
-            
-            # Create tokens
-            #import pdb; pdb.set_trace()
-            if tr['tr'] == t:
-                # If T_I has written, create write token
-                if write_str in tr['command']:
-                    write_token.append(tr['obj'])
-                # If T_I has read, create read token
-                if read_str in tr['command']:
-                    read_token.append(tr['obj'])
-            
-            # Check against tokens
-            if tr['tr'] != t:
-                # check for write -> read
-                if read_str in tr['command'] and tr['obj'] in write_token:
-                    graph[t] = t_not
-                # Check for read -> write        
-                if write_str in tr['command'] and tr['obj'] in read_token:
-                    graph[t] = t_not
-                # Check for write -> write
-                if write_str in tr['command'] and tr['obj'] in write_token:
-                    graph[t] = t_not
-    return graph
-            
-
-def determine_serial(schedule, idx):
-    """
-    This function reorganizes the data into a form that allows us to build
-    a directed graph from it, and return the output of 'create_digraph()'.
-    """
-    sch = []
-    s = re.compile("(?<=\()(.*?)(?=\))") # find string between parentheses
-    for i_idx, item in enumerate(schedule):
-        
-        if read_str in item or write_str in item:
-            obj = s.search(item).group()
-            p = {
-                    'tr': item[:2],
-                    'schedule': idx,
-                    'command': item[3:],
-                    'order': i_idx + 1,
-                    'obj': obj
-                    }
-            sch.append(p)
-    sch = sorted(sch, key=lambda k: k['order'])
-    graph = create_digraph(sch)
-    return(graph, sch)
-        
 
 start_program()
